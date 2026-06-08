e357905c3a81f7b1dde4c491b994bcbb546ad10e
Mausimux <56847436+Mausimux@users.noreply.github.com>
2026-06-08 02:06:34 +0200

    kuma

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+	description = "QML devshell";
+
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+	};
+
+	outputs = { nixpkgs, ... }: let
+		system = "x86_64-linux";
+		pkgs = nixpkgs.legacyPackages.${system};
+	in {
+		devShells.${system}.default = pkgs.mkShell {
+			packages = with pkgs; [
+				kdePackages.qtdeclarative
+			];
+		};
+	};
+}
